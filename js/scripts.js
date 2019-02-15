55118cdab7fc0658a9d9bbec1a74311686af6c4e
Rees Alumasa <reesalumasa@gmail.com>
2019-02-15 16:09:23 +0300

    Add changes to nindex, css and js files

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,13 @@ var a=parseInt(prompt("Input a:"));
 var b=parseInt(prompt("Input b:"));
 var c=parseInt(prompt("Input c"));
 if(a+b<=c||a+c<=b||b+c<=a) {
-alert("NOT a triangle at all")
-}
-else if(a===b&&a===c&&b===c) {
-alert("Equilateral Triangle");
+  alert("NOT a triangle at all")
+} else if(a===b&&a===c&&b===c) {
+  alert("Equilateral Triangle");
 } else if(a===b&&a!==c||a===c&&a!==b||b===c&&b!==a) {
-alert("Isosceles Triangle");
+  alert("Isosceles Triangle");
 } else if(a!==b&&b!==c&&a!==c&&a+b>c&&a+c>b&&b+c>a) {
-alert("Scalene Triangle")
+  alert("Scalene Triangle")
 } else {
-alert("NOT a Triangle at all")
+  alert("NOT a Triangle at all")
 }
