8530e47510ce326b7cbb7fff8615d67763cc47b4
Rees Alumasa <reesalumasa@gmail.com>
2019-02-15 17:30:33 +0300

    Add changes to js and html files

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ var c=parseInt(prompt("Input c"));
 if(a+b<=c||a+c<=b||b+c<=a) {
   alert("NOT a triangle at all")
 } else if(a===b&&a===c&&b===c) {
-  alert("Your values" + a+","+b+",and,"+c + "form an:..."+"Equilateral Triangle!");
+  alert("Your values " + a+" , "+b+" ,and ,"+c + " form an... "+"Equilateral Triangle!");
 } else if(a===b&&a!==c||a===c&&a!==b||b===c&&b!==a) {
-  alert("Your values" + a+","+b+",and,"+c + "form an:..."+"<Isosceles Triangle!");
+  alert("Your values " + a+" , "+b+", and ,"+c + " form an... "+"<Isosceles Triangle!");
 } else if(a!==b&&b!==c&&a!==c&&a+b>c&&a+c>b&&b+c>a) {
-  alert("Your values" + a+","+b+",and,"+c + "form a:..."+"Scalene Triangle!")
+  alert("Your values " + a+" , "+b+", and ,"+c + "form a... "+"Scalene Triangle!")
 } else {
   alert("NOT a Triangle at all")
 }
