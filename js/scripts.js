300d9e8ed19bb5b768ccfccce8d8da09e85c821c
Rees Alumasa <reesalumasa@gmail.com>
2019-02-18 10:52:13 +0300

    Add initial changes to js and index files

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 function tap() {
-var input=function(a,b, c) {
-return a, b, c
-}
-var a=parseInt("Input a:");
-var b=parseInt("Input b:");
-var c=parseInt("Input c");
+var a = document.getElementById("data1").value;
+var b = document.getElementById("data2").value;
+var c = document.getElementById("data3").value;
+var a=parseInt(a);
+var b=parseInt(b);
+var c=parseInt(c);
+console.log(a)
+console.log(b)
+console.log(c)
 if(a+b<=c||a+c<=b||b+c<=a) {
   alert("NOT a triangle at all")
 } else if(a===b&&a===c&&b===c) {
