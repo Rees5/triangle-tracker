87eb0cbd9579f23dd27eefa49262947601733834
Rees Alumasa <reesalumasa@gmail.com>
2019-02-18 10:32:29 +0300

    Add initial changes to js and index files

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+function tap() {
 var input=function(a,b, c) {
 return a, b, c
 }
-var a=parseInt(prompt("Input a:"));
-var b=parseInt(prompt("Input b:"));
-var c=parseInt(prompt("Input c"));
+var a=parseInt("Input a:");
+var b=parseInt("Input b:");
+var c=parseInt("Input c");
 if(a+b<=c||a+c<=b||b+c<=a) {
   alert("NOT a triangle at all")
 } else if(a===b&&a===c&&b===c) {
@@ -14,4 +15,5 @@ if(a+b<=c||a+c<=b||b+c<=a) {
   alert("Your values  " + a+",  "+b+"  and  "+c + "  form  a    "+"Scalene Triangle!")
 } else {
   alert("NOT a Triangle at all")
+}
 }
